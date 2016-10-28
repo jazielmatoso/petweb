72f4e2b567d92cbe7467b25f70a85dfc1da010a5
jazielmatoso <jazielsmatoso@gmail.com>
2016-10-28 21:45:13 -0200

    classes basicas

---
+++ ClassLibrary2/Basicas/Especie.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Basicas
+{
+    public class Especie
+    {
+        private int codEspecie;
+        private string nome;
+        private string descricao;
+
+        public Especie(int codEspecie, string nome, string descricao)
+        {
+            this.codEspecie = codEspecie;
+            this.nome = nome;
+            this.descricao = descricao;
+        }
+
+        public int CodEspecie
+        {
+            get
+            {
+                return codEspecie;
+            }
+
+            set
+            {
+                codEspecie = value;
+            }
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+
+            set
+            {
+                nome = value;
+            }
+        }
+
+        public string Descricao
+        {
+            get
+            {
+                return descricao;
+            }
+
+            set
+            {
+                descricao = value;
+            }
+        }
+
+      
+    }
+}
--- ClassLibrary2/Basicas/Usuario.cs
+++ ClassLibrary2/Basicas/Usuario.cs
@@ -8,13 +8,99 @@ namespace Backend.Basicas
 {
     public class Usuario
     {
-        private int id;
+        private int codUsuario;
+        private string nome;
+        private string telefone;
+        private string login;
+        private string senha;
+        private Permissao permissao;
+ 
 
-        public int? Id {get;set;}
-        
-        private String nome;
 
-        public String Nome { get; set; }
-        
+        public Usuario() {
+
+
+            this.Permissao = new Permissao();
+
+        }
+
+
+        public int CodUsuario
+        {
+            get
+            {
+                return codUsuario;
+            }
+
+            set
+            {
+                codUsuario = value;
+            }
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return nome;
+            }
+
+            set
+            {
+                nome = value;
+            }
+        }
+
+        public string Telefone
+        {
+            get
+            {
+                return telefone;
+            }
+
+            set
+            {
+                telefone = value;
+            }
+        }
+
+        public string Login
+        {
+            get
+            {
+                return login;
+            }
+
+            set
+            {
+                login = value;
+            }
+        }
+
+        public string Senha
+        {
+            get
+            {
+                return senha;
+            }
+
+            set
+            {
+                senha = value;
+            }
+        }
+
+        public Permissao Permissao
+        {
+            get
+            {
+                return permissao;
+            }
+
+            set
+            {
+                permissao = value;
+            }
+        }
     }
 }
